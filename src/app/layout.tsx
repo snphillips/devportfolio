1c8d60e61c87e7b0d3fc5ddf88f7716333c18165
Sarah Phillips <snphillips@gmail.com>
2025-06-29 10:58:18 -0400

    favicon & styles display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,16 @@
+import '../index.css';
+import { Oswald } from 'next/font/google';
+
+const oswald = Oswald({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700'],
+  display: 'swap',
+});
+
+export const metadata = {
+  title: 'Sarah Phillips Software Developer',
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -5,9 +18,47 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <div id="root">{children}</div>
-      </body>
+      <head>
+        {/* Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
+
+        {/* Favicons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="msapplication-TileImage"
+          content="/favicon/ms-icon-144x144.png"
+        />
+      </head>
+
+      <body className={oswald.className}>{children}</body>
     </html>
   );
 }
